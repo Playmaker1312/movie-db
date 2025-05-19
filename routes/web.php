eb5f4f995ed93ba9ca1110e9675c8f66693cfcef
zaki zafran <122102806+Playmaker1312@users.noreply.github.com>
2025-05-19 16:21:56 +0700

    template and layouts

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Models\Movie;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+    Route::get('/', [MovieController::class, 'homepage'])->name('homepage');
